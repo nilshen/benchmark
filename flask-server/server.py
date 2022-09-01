98abb68b854d569cd9524d7aff6471a544ba3639
nilshen <linshen88@gmail.com>
2022-08-31 22:35:27 -0400

    function done - added overall score

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,68 +1,83 @@
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
+import requests
+from io import StringIO
 
 ###combine two source files into one joined
-score = 'score-records.csv'
-company = 'companies.csv'
-score = pd.read_csv(score)
-company = pd.read_csv(company)
+score = requests.get('https://s3.amazonaws.com/simple-fractal-recruiting/score-records.csv')
+company = requests.get('https://s3.amazonaws.com/simple-fractal-recruiting/companies.csv')
+# print(scores.text)
+score = pd.read_csv(StringIO(score.text))
+company = pd.read_csv(StringIO(company.text))
 joined = pd.merge(score, company[["company_id","fractal_index"]], on="company_id", how="left")
-# print(joined)
+### I decided to add an additional column as overvall score which has same weight for communication & coding scores. 
+### You will be surprised that overall percentile can be higher than both. refer to 914 for example.
+joined["overall"] = joined["communication_score"] + joined["coding_score"]
 
+### first try with local download. Ultimized with loading directly from aws to reduce server load (refer to above).
+# score = 'score-records.csv'
+# company = 'companies.csv'
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     # if request.method == 'POST':
-#         # id = request.form['id']
-#     return [coding_percentile, com_percentile]
-
-# @app.route("/<int:id>")
-# def percentiles(id):
-
 @app.route("/<int:id>")
 def percentiles(id):
-    search_id = id
-    candidate = joined.query("candidate_id == @search_id")
+    candidate = joined.query("candidate_id == @id")
 
     ###filtered the data with same candidate title and similar company fractal index range
     candidate_title = candidate["title"].values[0]
     candidate_fractal = candidate["fractal_index"].values[0]
     filtered = joined.loc[(joined["title"] == candidate_title) & (joined["fractal_index"] > candidate_fractal - 0.15) & (joined["fractal_index"] < candidate_fractal + 0.15)]
+    ## testing
     # print(candidate['fractal_index'])
     # print(candidate['title'])
     # print(filtered)
 
-
-    ###alternative way to calculate percentile based on filtered data
+    ###calculate percentile based on filtered score for communication, coding, and overall
     com_filtered = filtered.sort_values(by=["communication_score"], ascending = 1)
-    com_total = len(com_filtered["candidate_id"]) - 1 #needs to minus 1 because total length excluding self.
+    com_total = len(com_filtered["candidate_id"]) - 1 #needs to minus 1 total length excluding self.
     com_less_than = list(com_filtered["candidate_id"].values).index(id)
 
     com_percentile = str(round(float(com_less_than / com_total * 100), 1))
-
+    
+    ## testing
     # print()
     # print(com_less_than)
     # print(com_total)
     # print(com_percentile)
 
     coding_filtered = filtered.sort_values(by=["coding_score"], ascending = 1)
-    coding_total = len(coding_filtered["candidate_id"]) - 1 #needs to minus 1 because total length excluding self.
+    coding_total = len(coding_filtered["candidate_id"]) - 1 #needs to minus 1  total length excluding self.
     coding_less_than = list(coding_filtered["candidate_id"].values).index(id)
 
     coding_percentile = str(round(float(coding_less_than / coding_total * 100), 1))
+    
+    ## testing
     # print()
     # print(coding_less_than)
     # print(coding_total)
     # print(coding_percentile)
 
+
+    overall_filtered = filtered.sort_values(by=["overall"], ascending = 1)
+    overall_total = len(overall_filtered["candidate_id"]) - 1 #needs to minus 1 total length excluding self.
+    overall_less_than = list(overall_filtered["candidate_id"].values).index(id)
+
+    overall_percentile = str(round(float(overall_less_than / overall_total * 100), 1))
+
+    ##testing
+    # print()
+    # print(overall_less_than)
+    # print(overall_total)
+    # print(overall_percentile)
+
     # print(com_filtered)
     # print(coding_filtered)
+    # print(overall_filtered)
 
-    return [com_percentile+'%', coding_percentile+'%']
+    return [com_percentile+"%", coding_percentile+"%", overall_percentile+"%"]
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost', port=5000)
+    app.run(debug=True, host="localhost", port=5000)
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { FiSearch } from "react-icons/fi"
-import {Link} from 'react-router-dom';
 
 function App() {
 
   const[id, setId] = useState('')
+  const[comPercentile, setComPercentile] = useState('')
+  const[codingPercentile, setCodingPercentile] = useState('')
+  const[overallPercentile, setOverallPercentile] = useState('')
+
   console.log(id)
 
   const handleClick = () => {
-    fetch(`/${id}`, {
-    })
-      .then(response => response.json()
-      .then(data => ({ data, response })))
-      .then(({ data, response }) =>  {
-        document.getElementById("com_percentile").innerHTML = data[0]
-        document.getElementById("coding_percentile").innerHTML = data[1]
+    fetch(`/${id}`)
+      .then(response => {
+        // testing
+        // console.log(typeof response)
+        return response.json()
+      })
+      .then(data =>  {
+        setComPercentile(data[0])
+        setCodingPercentile(data[1])
+        setOverallPercentile(data[2])
+      
+        /// inital try - set additional state instead of updating the innerHTML tags
+        // document.getElementById("com_percentile").innerHTML = data[0]
+        // document.getElementById("coding_percentile").innerHTML = data[1]
         
-        console.log(data[0])
-        console.log(data[1])
+        // testing
+        // console.log(data[0])
+        // console.log(data[1])
+        // console.log(data)
   })}
     
     return (
-    <div>
-      <header>Welcome to the Benchmark Website, where you can check your percentile scores</header>
+    <div className='page_container'>
+      <header>Welcome to the Benchmark Website, where you can check your percentile against programmers with the same title and from similar companies</header>
       <br/>
           <div>Please enter your Candidate ID:   
             <input 
@@ -38,14 +50,16 @@ function App() {
         <br/>
         <div>
           Communication Score Percentile: 
-          <div id='com_percentile'></div>
+          <div id='com_percentile'>{comPercentile}</div>
         </div>
         <div>
           Coding Score Percentile: 
-          <div id='coding_percentile'></div>
+          <div id='coding_percentile'>{codingPercentile}</div>
         </div>
-       
-
+        <div>
+          Overall Score Percentile: 
+          <div id='coding_percentile'>{overallPercentile}</div>
+        </div>
     </div>
   )
 }
