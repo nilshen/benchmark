eff655430b5f6332763d1bab670957dea98d4c53
nilshen <linshen88@gmail.com>
2022-09-01 01:05:58 -0400

    css polished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,78 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { FiSearch } from "react-icons/fi" 
-import './index.css'
+import React, { useState } from 'react';
+import { FiSearch } from "react-icons/fi";
+// import './index.css'
 
 function App() {
 
-  const[id, setId] = useState('')
-  const[comPercentile, setComPercentile] = useState('')
-  const[codingPercentile, setCodingPercentile] = useState('')
-  const[overallPercentile, setOverallPercentile] = useState('')
+  const [id, setId] = useState('');
+  const [comPercentile, setComPercentile] = useState('');
+  const [codingPercentile, setCodingPercentile] = useState('');
+  const [overallPercentile, setOverallPercentile] = useState('');
 
-  console.log(id)
+  console.log(id);
 
   const handleClick = () => {
     fetch(`/${id}`)
       .then(response => {
         // testing
         // console.log(typeof response)
-        return response.json()
+        return response.json();
       })
-      .then(data =>  {
-        setComPercentile(data[0])
-        setCodingPercentile(data[1])
-        setOverallPercentile(data[2])
-      
+      .then(data => {
+        setComPercentile(data[0]);
+        setCodingPercentile(data[1]);
+        setOverallPercentile(data[2]);
+        setId('');
+
+        document.getElementById("com_percentile").style.display = 'block';
+        document.getElementById("coding_percentile").style.display = 'block';
+        document.getElementById("overall_percentile").style.display = 'block';
+
         /// inital try - set additional state instead of updating the innerHTML tags
         // document.getElementById("com_percentile").innerHTML = data[0]
         // document.getElementById("coding_percentile").innerHTML = data[1]
-        
+
         // testing
         // console.log(data[0])
         // console.log(data[1])
         // console.log(data)
-  })}
-    
-    return (
+      })
+      .catch(console.error);
+  };
+
+  return (
     <div className='page-container'>
       <div className='header'>
         Welcome to the Benchmark, where you can check your percentile against programmers with the same title and from similar companies
       </div>
-      <br/>
-      
+      <br />
+
       <div className='body-container'>
-        <div className='input-title'>   
+        <div className='search-container'>
           <input className='input'
-            type ="text"
+            type="text"
             // name ="candidate_id" 
             placeholder="Please enter your candidate ID..."
             onChange={e => setId(e.target.value)}
             value={id}
-            />
-            <FiSearch onClick={handleClick} className="searchbar-icon"/>
+          />
+          <FiSearch onClick={handleClick} className="searchbar-icon" />
         </div>
 
-        <br/>
+        <br />
         <div className='percentiles-container'>
+
           <div className='percentiles-row'>
-            <div className='percentiles-title'>Communication Percentile: </div> 
-            <div className='percentile-result' id='com_percentile'>{comPercentile}</div>
+            <div id='com_percentile'>Communication Percentile: </div>
+            <div className='percentile-result'>{comPercentile}</div>
           </div>
           <div className='percentiles-row'>
-            <div className='percentiles-title'>Coding Percentile: </div> 
-            <div className='percentile-result' id='coding_percentile'>{codingPercentile}</div>
+            <div id='coding_percentile'>Coding Percentile: </div>
+            <div className='percentile-result'>{codingPercentile}</div>
           </div>
           <div className='percentiles-row'>
-            <div className='percentiles-title'>Overall Percentile: </div> 
-            <div className='percentile-result' id='overall_percentile'>{overallPercentile}</div>
+            <div id='overall_percentile'>Overall Percentile: </div>
+            <div className='percentile-result'>{overallPercentile}</div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 
 
